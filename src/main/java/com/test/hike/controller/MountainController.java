244b2a11e384861c55c92a4ca8dc281739553ea0
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2024-11-18 18:43:39 +0900

    Chore: 산 개별페이지 컨트롤러 doc 작성

--- src/main/java/com/test/hike/controller/MountainController.java
+++ src/main/java/com/test/hike/controller/MountainController.java
@@ -4,11 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * CourseController.java
+ * 산 개별 페이지와 관련된 Controller입니다.
+ * @author Jeong Gwan-woo
+ */
 @Controller
 public class MountainController {
 
+    /**
+     * 전국의 산을 선택하는 코스 페이지를 출력하는 method 입니다.
+     * @param mtId 사용자가 선택한 산의 고유번호
+     * @return 산 선택 View 페이지
+     */
     @GetMapping("mountain")
-    public String index() {
+    public String index(String mtId) {
         return "mountain";
     }
 }
