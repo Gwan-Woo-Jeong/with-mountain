c7dfb19548943ae4298278143d4ba3e34d732e7e
DavidDawnroad <cypoverhire@gmail.com>
2024-11-18 19:26:55 +0900

    등산로 커스텀 관련 Javadoc 작업 Controller/DAO/DTO/Mapper/JavaScript 완료

--- src/main/java/com/test/hike/dto/custom/CoordDTO.java
+++ src/main/java/com/test/hike/dto/custom/CoordDTO.java
@@ -2,6 +2,11 @@ package com.test.hike.dto.custom;
 
 import lombok.Data;
 
+/**
+ * 등산로 정보가 들어갈 DTO 입니다.
+ * 등산로 구간의 좌표와 일련번호 정보가 포함되어 있습니다.
+ * @author Kim Young-jin, Jeong Gwan-woo
+ */
 @Data
 public class CoordDTO {
 	private int coordId;
--- src/main/java/com/test/hike/mapper/MapCustomMapper.java
+++ src/main/java/com/test/hike/mapper/MapCustomMapper.java
@@ -10,13 +10,17 @@ import com.test.hike.dto.HikingRoadDTO;
 import com.test.hike.dto.HikingRoadSpotDTO;
 import org.apache.ibatis.annotations.Param;
 
+/**
+ * 커스텀 등산로 기능과 관련된 Interface Mapper 파일입니다.
+ * 실제 SQL Query는 동일한 이름의 xml 파일에 구현되었습니다.
+ * @author Kim Young-jin, Jeong Gwan-woo
+ */
 @Mapper
 public interface MapCustomMapper {
 	
 	List<HikingRoadSpotDTO> getHikingRoadSpots(); // DB에서 등산로 지점 데이터 전부를 가져오는 method
 
 	List<MountainDTO> getSearchMountainSpots(); // 산 위치
-
 
 	List<HikingRoadDTO> getHikingRoads();
 
