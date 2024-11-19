a0e7a55a628a47f40602250b2663cd4f4cb45c4c
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2024-11-19 22:33:56 +0900

    Docs: LocatioController Javadoc 작성

--- src/main/java/com/test/hike/controller/LocationController.java
+++ src/main/java/com/test/hike/controller/LocationController.java
@@ -11,24 +11,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.test.hike.dto.LocationDTO;
 import com.test.hike.service.LocationService;
 
+/**
+ * LocationController.java
+ * 활동 지역 관련 요청을 처리하는 Controller입니다.
+ * 회원가입 폼 및 지역 정보를 제공하는 메서드를 포함합니다.
+ * @author Lee Hye-mi
+ */
 @Controller
 public class LocationController {
-    
+
     @Autowired
     private LocationService locationService;
-    
-    // 회원가입 폼에서 사용할 수 있도록 UserController나 해당하는 컨트롤러에서 처리하는 것이 좋음
+
+    /**
+     * 회원가입 폼을 표시하는 메서드입니다.
+     *
+     * @param model 회원가입 폼에 필요한 데이터를 담기 위한 모델 객체
+     * @return 회원가입 폼 JSP 경로
+     */
     @GetMapping("/signup")  // URL 수정
     public String showSignupForm(Model model) {
         model.addAttribute("locations", locationService.getAllLocations());
         return "user/signup";  // 실제 회원가입 폼이 있는 JSP 경로
     }
-    
-    //회원가입 모달이 열릴 때 지역 데이터를 가져와서 select 박스에 표시
+
+    /**
+     * 회원가입 모달이 열릴 때 지역 데이터를 가져와서 select 박스에 표시하는 메서드입니다.
+     *
+     * @return List&lt;LocationDTO&gt; 모든 활동 지역 정보의 리스트
+     */
     @GetMapping("/getAllLocations.do")
     @ResponseBody
     public List<LocationDTO> getAllLocations() {
         return locationService.getAllLocations();
     }
-    
 }
